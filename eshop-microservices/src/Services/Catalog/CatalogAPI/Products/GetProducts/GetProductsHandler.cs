a882539bc70e334030d56281d29d8c95c346b4b4
Guillermo <gkaveckis@gmail.com>
2026-07-28 12:23:55 -0300

    Initial solution

---
+++ eshop-microservices/src/Services/Catalog/CatalogAPI/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,18 @@
+﻿using Marten;
+
+namespace CatalogAPI.Products.GetProducts
+{
+    public record GetProductsQuery(): IQuery<GetProductsResult>;   
+    public record GetProductsResult(IEnumerable<Product> Products);
+    public class GetProductsQueryHandler(IQuerySession session, ILogger<GetProductsQueryHandler> logger) : IQueryHandler<GetProductsQuery, GetProductsResult>
+    {
+        public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
+
+            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+
+            return new GetProductsResult(products);
+        }
+    }
+}
---
+++ eshop-microservices/src/Services/Catalog/CatalogAPI/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR(cfg => 
+{ 
+    cfg.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);    
+}).UseLightweightSessions();
+
+var app = builder.Build();
+
+app.MapCarter();
+
+app.Run();
